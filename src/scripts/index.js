70575c48abdfb04762c135fc5f3c3f6b9fce423e
alice <ajsc1@aluno.ifal.edu.brm>
2023-11-02 18:35:02 -0300

    -m

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,11 +16,43 @@ const icons = [
 ];
 
 let shuffleIcons = icons.sort(() => (Math.random() > 0.5 ? 2 : -1));
+let openCards = [];
 
 for (let i = 0; i < icons.length; i++) {
   let box = document.createElement("div");
   box.className = "card";
   box.innerHTML = shuffleIcons[i];
+  box.onclick = handleClick;
   document.querySelector(".game").appendChild(box);
-  console.log(box.className);
+}
+
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add("open");
+    openCards.push(this);
+  }
+
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
+
+function checkMatch() {
+  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add("match");
+    openCards[1].classList.add("match");
+  } else {
+    openCards[0].classList.remove("open");
+    openCards[1].classList.remove("open");
+  }
+
+  openCards = [];
+
+  checkWin();
+}
+
+function checkWin() {
+  if (document.querySelectorAll(".match").length === icons.length) {
+    alert("YOU WIN!");
+  }
 }
